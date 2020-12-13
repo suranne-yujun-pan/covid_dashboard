736ef0b30fd311ff179899fcce6a128d555ae171
suranne_yujun_pan <yujun.pan@outlook.com>
2020-12-13 12:16:48 -0500

    commit

--- data/get_data.R
+++ data/get_data.R
@@ -9,3 +9,22 @@ setwd("/Users/panyujun/Documents/Portfolio/covid_dashboard/data")
 write.csv(countries_ts, "countries_ts.csv", row.names = FALSE)
 write.csv(countries_ts_c, "countries_ts_c.csv", row.names = FALSE)
 write.csv(pop, "pop.csv", row.names = FALSE)
+
+spdata <- countries_ts_c %>% 
+  filter(Date == max(Date)
+  ) %>% 
+  filter(!is.na(positive)) %>% 
+  mutate(Country.Region = as.character(Country.Region)) %>% 
+  full_join(., pop, by = c("Country.Region" = "Location"))%>% 
+  mutate(over_65_per_population = pop_over_65/population*100,
+         cases_per_population = positive/population,
+         death_rate = deaths/positive*100
+  )
+
+write.csv(spdata, "spdata.csv", row.names = FALSE)
+
+# country_list <- countries_ts_c %>% 
+#   filter(Date == max(Date)) %>% 
+#   arrange(desc(positive)) %>% 
+#   slice(1:50) %>% 
+#   select(Country.Region) %>% pull()
